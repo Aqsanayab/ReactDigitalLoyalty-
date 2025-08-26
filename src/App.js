07b8ffaaac5e3fc047504d48fe0815777f097496
Aqsanayab <aqsanayab5@gmail.com>
2025-08-26 07:18:55 -0700

    again chabge app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import AdminDashboard from "./pages/AdminDashboard";
@@ -9,10 +8,15 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Default: root par Dashboard */}
         <Route path="/" element={<Dashboard />} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/adminDashboard" element={<AdminDashboard />} />
+
+        {/* Agar koi unknown link open kare to bhi Dashboard */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
