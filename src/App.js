abaa1fbed31f58538adc31b255a248f5522ad7ef
Aqsanayab <aqsanayab5@gmail.com>
2025-08-26 07:14:21 -0700

    Changed BrowserRouter to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -8,11 +8,11 @@ import AdminDashboard from "./pages/AdminDashboard";
 function App() {
   return (
     <Router>
-          <Routes>
-        <Route path="/" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
-         <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/adminDashboard" element={<AdminDashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/adminDashboard" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
